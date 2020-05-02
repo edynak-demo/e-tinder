59cd9a85a2fdb9b42411e373e7fb54b7de761256
edynak-demo <edynakdemo@gmail.com>
2020-05-02 11:03:31 -0400

    intialized app and implemented custom sidebar menu

---
+++ www/js/app.js
@@ -0,0 +1,53 @@
+angular
+  .module("starter", ["ionic"])
+
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.Keyboard) {
+        window.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state("app", {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+      })
+
+      .state("app.profile", {
+        url: "/profile",
+        views: {
+          menuContent: {
+            templateUrl: "templates/profile.html",
+          },
+        },
+      })
+      .state("app.home", {
+        url: "/home",
+        views: {
+          menuContent: {
+            templateUrl: "templates/home.html",
+          },
+        },
+      })
+
+      .state("app.settings", {
+        url: "/settings",
+        views: {
+          menuContent: {
+            templateUrl: "templates/settings.html",
+          },
+        },
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise("/app/home");
+  });
